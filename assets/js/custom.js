b94919dda0f50080e26dae9b354750341a919140
Yi Rui <yrui@sankyinc.com>
2017-06-26 11:58:16 -0400

    add badges

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -179,4 +179,46 @@
 
 	});
 
+  $(function() {
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/yrui.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      populateWithCourses(response.courses.completed)
+    }
+  });
+
+  function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course col-sm-4 text-center'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
+
+});
+
+
 })(jQuery);
